61541ffb6a3e6a8399481f5a7fe7bdc10362036a
Adrian Dupuis <adrcdupuis@gmail.com>
2025-08-31 10:02:30 +0000

    Commit with staging updated

--- models/1_staging/localbike/stg_localbike__staffs.sql
+++ models/1_staging/localbike/stg_localbike__staffs.sql
@@ -1,10 +1,10 @@
 SELECT
     staff_id,
-    first_name,
-    last_name,
-    email,
-    phone,
-    active,
+    first_name AS staff_first_name,
+    last_name AS staff_last_name,
+    email AS staff_email,
+    phone AS staff_phone,
+    active AS staff_active,
     store_id,
     manager_id
 FROM
